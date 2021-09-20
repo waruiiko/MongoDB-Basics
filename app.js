e9d1789678ebdf6e04ba5d4c86aed1393a60581d
GinBe <asdfwxwx@gmail.com>
2021-09-20 11:10:28 +0800

    app.js

--- app.js
+++ app.js
@@ -1,13 +1,44 @@
 const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://m001-student:<m001-mongodb-basics>@sandbox.yiyle.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.yiyle.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-    console.log("1")
+// client.connect(err => {
+//     console.log("1")
+//     const collection = client.db("sample_mflix").collection("movies");
+//     // console.log(collection)
+//     // perform actions on the collection object
+//     const query = { title: 'Back to the Future' };
+//     const movie = collection.findOne(query);
+//     console.log(movie);
+//     client.close();
+//     console.log("3")
+// });
 
-    const collection = client.db("test").collection("devices");
-    console.log(collection)
-    // perform actions on the collection object
-    client.close();
-    console.log("3")
-});
+async function run() {
+    try {
+        await client.connect();
+        // const database = client.db('sample_training');
+        // const movies = database.collection('zips');
+        const collection = client.db("sample_training").collection("zips");
+        // Query for a movie that has the title 'Back to the Future'
+        const query = { city: 'ALPINE' };
+        const movie = await collection.findOne(query);
+        console.log(movie);
+        // await client.connect(err => {
+        //     console.log("1")
+        //     // const collection = client.db("test").collection("devices");
+        //     const collection = client.db("sample_training").collection("zips");
+        //     // console.log(collection)
+        //     // perform actions on the collection object
+        //     const query = { city: 'ALPINE' };
+        //     const movie = collection.findOne(query);
+        //     console.log(movie);
+        //     client.close();
+        //     console.log("3")
+        // });
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
 console.log("nice")
---
+++ index.js
@@ -0,0 +1,21 @@
+const { MongoClient } = require("mongodb");
+// Replace the uri string with your MongoDB deployment's connection string.
+const uri =
+"mongodb+srv://m001-student:m001-mongodb-basics@sandbox.yiyle.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  // "mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&writeConcern=majority";
+const client = new MongoClient(uri);
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db('sample_mflix');
+    const movies = database.collection('movies');
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { title: 'Back to the Future' };
+    const movie = await movies.findOne(query);
+    console.log(movie);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
