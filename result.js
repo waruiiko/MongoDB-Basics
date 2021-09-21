df1c4da74dbdcd935ca1f1b8117230f5685fd6d3
GinBe <asdfwxwx@gmail.com>
2021-09-22 05:56:38 +0800

    Update result.js

--- result.js
+++ result.js
@@ -1,18 +1,2 @@
 import run from './test.js'
 run()
-// const runs = run()
-// console.log(runs)
-// async function findDocuments (db) {
-//     // Get the documents collection
-//     const collection = client.db("sample_training").collection("trips");
-  
-//     // Insert some documents
-//     const query = {
-//         "tripduration": { "$lte": 70 },
-//         "usertype": "Customer"
-//     };
-//     const movie = await collection.findOne(query);
-//     console.log(movie);
-  
-//     return movie
-//   }
