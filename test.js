4b37aadb1a3dd994e3f2d04c883de3c6c33a40d4
GinBe <asdfwxwx@gmail.com>
2021-09-29 15:55:23 +0800

    test

--- test.js
+++ test.js
@@ -9,18 +9,37 @@ async function run() {
         // console.log(db);
         // const database = client.db('sample_training');
         // const movies = database.collection('zips');
-        const collection =client.db("sample_training").collection("zips");
+        const collection = client.db("sample_training").collection("companies");
         const query = {
             // "birth year": { "$eq": 1998 }
 
             //人口大于5000小于1000000
-            "$nor":[{"pop":{"$lt":5000}},{"pop":{"$gt":1000000}}]
+            // "$nor": [{ "pop": { "$lt": 5000 } }, { "pop": { "$gt": 1000000 } }]
+
+            //either founded in 2004
+            // [and] either have the social category_code [or] web category_code,
+            // [or] were founded in the month of October    
+            // [and] also either have the social category_code [or] web category_code?
+            "$or": [
+                {
+                    "$and": [{ "founded_year": 2004 }, {
+                        "$or": [{ "category_code": "social" },
+                        { "category_code": "web" }]
+                    }]
+                },
+                {
+                    "$and": [{ "founded_month": 10 }, {
+                        "$or": [{ "category_code": "social" },
+                        { "category_code": "web" }]
+                    }]
+                },
+            ]
         };
         const movie = await collection.find(query).count()
         console.log(movie);
     } catch (err) {
         // Handle error
-        console.log("err",err)
+        console.log("err", err)
     } finally {
         // Ensures that the client will close when you finish/error
         await client.close();
